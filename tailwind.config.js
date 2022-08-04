3eacabfc5bf29ab93c0874c64cf2dbabf443ea54
jennyfielding12 <100756172+jennyfielding12@users.noreply.github.com>
2022-08-04 06:12:47 +0100

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-/** @type {import('tailwindcss').Config} */ 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
